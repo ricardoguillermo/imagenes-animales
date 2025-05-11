654c55335f000457dcf7152acc53fbaf9c3e0de4
Ricardo Nario <diguibal@gmail.com>
2025-05-11 18:06:50 -0300

    Corregido error de defineConfig duplicado

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-/* export default defineConfig({
-  server: {
-    host: "0.0.0.0",
-    port: process.env.PORT || 5173,
-  },
-}); */
-
-import { defineConfig } from "vite";
-
 export default defineConfig({
   preview: {
     host: "0.0.0.0",
