e9584e1176ad7b21dcc5586d40e8fe65d28ce769
Ricardo Nario <diguibal@gmail.com>
2025-05-11 12:37:25 -0300

    Permitir dominio de Render

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,24 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
+/* export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 5173,
+  },
+}); */
+
+import { defineConfig } from "vite";
+
 export default defineConfig({
+  preview: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 4173,
+    allowedHosts: ["imagenes-animales.onrender.com"],
+  },
   server: {
     host: "0.0.0.0",
     port: process.env.PORT || 5173,
   },
 });
+// import react from "@vitejs/plugin-react";
