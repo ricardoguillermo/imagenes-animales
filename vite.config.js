76ebd40a9e72ebe3b87aa7aabcbf2ab7348636b8
Ricardo Nario <diguibal@gmail.com>
2025-05-12 08:55:21 -0300

    arreglando

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 5173,
+  },
   preview: {
     host: "0.0.0.0",
     port: process.env.PORT || 4173,
     allowedHosts: ["imagenes-animales.onrender.com"],
   },
-  server: {
-    host: "0.0.0.0",
-    port: process.env.PORT || 5173,
-  },
 });
-// import react from "@vitejs/plugin-react";
